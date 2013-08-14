28ebc18aadd0670b548fdbf0a8d600e1e5319288
Tom Aizenberg <tom.aizenberg@gmail.com>
2013-08-14 17:59:32 +0200

    Update docs

--- lib/timed_report.rb
+++ lib/timed_report.rb
@@ -1,5 +1,12 @@
 class TimedReport
-  
+
+  # Creates a new TimedReport instance. If _named_ is given it will show the name in the report.
+  #
+  # Example:
+  #   >> tr = TimedReport.new("Test")
+  #
+  # Arguments:
+  #   named: (String)
   def initialize named = nil
     @start_time = Time.now
     @step_time = Time.now
@@ -13,14 +20,35 @@ class TimedReport
     @output_methods = [lambda{|m| puts(m)}]
   end
 
+  # Add a Proc or lambda that takes the report as argument.
+  #
+  # This way you can for example add Rails.logger.info or send an email besides printing to STDOUT.
+  #
+  # Example:
+  #   >> tr.add_output_method(lambda{|report| Dummy.send_email(report)})
+  #
+  # Arguments:
+  #   method: (Proc or lambda)
   def add_output_method method
     @output_methods.push method
   end
 
+  # Increments time when called.
+  # 
+  # Example:
+  #   >> tr.time_step()
   def time_step
     @step_time = Time.now
   end
 
+  # Adds a line of text including the difference in seconds between the last #add or #time_step.
+  #
+  # Example:
+  #   >> tr.add("one step")
+  # 
+  # Arguments:
+  #   txt: (String)
+  #   with_time: (Boolean)
   def add txt, with_time = true
     if with_time
       timing_info = "%.5f: " % (Time.now - @step_time)
@@ -32,6 +60,10 @@ class TimedReport
     @output += "\n#{timing_info}#{txt}"
   end
 
+  # Finish the report. Call this add the end to print the report!
+  # 
+  # Example:
+  #   >> tr.finish()
   def finish
     @output += "\n--------------------------"
     @output += "\nTotal time: %.5f" % (Time.now-@start_time)
